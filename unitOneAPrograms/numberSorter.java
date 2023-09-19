3545a48f721adf80a7a28ca587b647f806979052
NTechStorm <72469826+NTechStorm@users.noreply.github.com>
2023-09-19 16:48:48 -0700

    Update numberSorter.java

--- unitOneAPrograms/numberSorter.java
+++ unitOneAPrograms/numberSorter.java
@@ -1,48 +1,44 @@
 import java.util.Scanner;
 
-public class numberSorter
-{
-    public static void main(String args[]) {
+public class NumberSorter {
+    public static void main(String[] args) {
         Scanner scanNumber = new Scanner(System.in);
-        System.out.println("Enter three numbers separated by a enter: ");
+        System.out.println("Enter three numbers separated by an enter: ");
         int[] numberArray = new int[3];
-        numberArray[0]= scanNumber.nextInt();
-        numberArray[1]= scanNumber.nextInt();
-        numberArray[2]= scanNumber.nextInt();
-        
+        numberArray[0] = scanNumber.nextInt();
+        numberArray[1] = scanNumber.nextInt();
+        numberArray[2] = scanNumber.nextInt();
+
         int[] numberSorted = new int[3];
-        
-        if (numberArray[0] < numberArray[1] && numberArray[1] < numberArray[2]) {
+
+        if (numberArray[0] <= numberArray[1] && numberArray[1] <= numberArray[2]) {
             numberSorted[0] = numberArray[0];
-            if (numberArray[1] < numberArray[2]) {
-                numberSorted[1] = numberArray[1];
-                numberSorted[2] = numberArray[2];
-            } else {
-                numberSorted[1] = numberArray[2];
-                numberSorted[2] = numberArray[1];
-            }
-        } else if (numberArray[1] < numberArray[0] && numberArray[1] < numberArray[2]) {
+            numberSorted[1] = numberArray[1];
+            numberSorted[2] = numberArray[2];
+        } else if (numberArray[0] <= numberArray[2] && numberArray[2] <= numberArray[1]) {
+            numberSorted[0] = numberArray[0];
+            numberSorted[1] = numberArray[2];
+            numberSorted[2] = numberArray[1];
+        } else if (numberArray[1] <= numberArray[0] && numberArray[0] <= numberArray[2]) {
             numberSorted[0] = numberArray[1];
-            if (numberArray[0] < numberArray[1]) {
-                numberSorted[1] = numberArray[0];
-                numberSorted[2] = numberArray[2];
-            } else {
-                numberSorted[1] = numberArray[2];
-                numberSorted[2] = numberArray[0];
-            }
-        } else if (numberArray[2] < numberArray[0] && numberArray[2] < numberArray[1]) {
+            numberSorted[1] = numberArray[0];
+            numberSorted[2] = numberArray[2];
+        } else if (numberArray[1] <= numberArray[2] && numberArray[2] <= numberArray[0]) {
+            numberSorted[0] = numberArray[1];
+            numberSorted[1] = numberArray[2];
+            numberSorted[2] = numberArray[0];
+        } else if (numberArray[2] <= numberArray[0] && numberArray[0] <= numberArray[1]) {
             numberSorted[0] = numberArray[2];
-            if (numberArray[0] < numberArray[1]) {
-                numberSorted[1] = numberArray[0];
-                numberSorted[2] = numberArray[1];
-            } else {
-                numberSorted[1] = numberArray[1];
-                numberSorted[2] = numberArray[0];
-            }
+            numberSorted[1] = numberArray[0];
+            numberSorted[2] = numberArray[1];
+        } else {
+            numberSorted[0] = numberArray[2];
+            numberSorted[1] = numberArray[1];
+            numberSorted[2] = numberArray[0];
         }
-        
+
         System.out.println("The integers from least to greatest are: " + numberSorted[0] + ", " + numberSorted[1] + ", " + numberSorted[2]);
-        
+
         scanNumber.close();
-    }   
+    }
 }
